93f7d85788ead3a3d337b043217822a5839f1130
Francky <gwadafrank@yahoo.com>
2018-04-10 13:04:48 +0300

    [Feature] Implement registration template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './static/App.css';
+import Registration from './components/Registration';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Registration />
       </div>
     );
   }
