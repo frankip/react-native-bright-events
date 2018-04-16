048c6bd86f6b9e5a8cab8faa0def3e64ac39fa29
Francky <gwadafrank@yahoo.com>
2018-04-16 03:11:38 +0300

    [feature] create a reusable navigation component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,23 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+// local imports
+import Navigation from './Navigation';
+
 class Main extends Component {
   render() {
     return (
       <div>
-        <nav className="nav">
-          <a className="toggle-nav" href="#">&#9776;</a>
-          <div className="nav-mobile">
-            <ul className="left">
-              <li><a href="index.html"><i className="fa fa-home"></i> Home</a></li>
-              <li><a href="#"><i className="fa fa-book"></i> Categories</a></li>
-              <li><a href="#"><i className="fa fa-fire"></i> Most Popular</a></li>
-            </ul>
-            <ul className="right">
-              <li> <a href="sign-in.html"><i className="fa fa-user"></i> Logout</a> </li>
-            </ul>
-          </div>
-        </nav>
+        <Navigation />
         <section className="row">
           <div id="eventCard" className="row small-up-1 medium-up-2 large-up-3">
             <div className="column">
---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="nav">
+          <a className="toggle-nav" href="#">&#9776;</a>
+          <div className="nav-mobile">
+            <ul className="left">
+              <li><a href="index.html"><i className="fa fa-home"></i> Home</a></li>
+              <li><a href="#"><i className="fa fa-book"></i> Categories</a></li>
+              <li><a href="#"><i className="fa fa-fire"></i> Most Popular</a></li>
+            </ul>
+            <ul className="right">
+              <li> <a href="sign-in.html"><i className="fa fa-user"></i> Logout</a> </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
