c40bf5c92275004d3972f99d1799f8fc4fc9e5cc
Francky <gwadafrank@yahoo.com>
2018-04-16 03:21:09 +0300

    [feature] Update navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,12 +10,24 @@ class Navigation extends Component {
           <a className="toggle-nav" href="#">&#9776;</a>
           <div className="nav-mobile">
             <ul className="left">
-              <li><a href="index.html"><i className="fa fa-home"></i> Home</a></li>
-              <li><a href="#"><i className="fa fa-book"></i> Categories</a></li>
-              <li><a href="#"><i className="fa fa-fire"></i> Most Popular</a></li>
+              <li>
+                <Link to="/events">Home</Link>
+                {/* <a href="index.html"><i className="fa fa-home"></i> Home</a> */}
+              </li>
+              <li>
+                <Link to="/events">Categories</Link>
+                {/* <a href="#"><i className="fa fa-book"></i> Categories</a> */}
+              </li>
+              <li>
+                <Link to="/events">Most Popular</Link>
+                {/* <a href="#"><i className="fa fa-fire"></i> Most Popular</a> */}
+              </li>
             </ul>
             <ul className="right">
-              <li> <a href="sign-in.html"><i className="fa fa-user"></i> Logout</a> </li>
+              <li>
+                <Link to="/login">logout</Link>
+                {/* <a href="sign-in.html"><i className="fa fa-user"></i> Logout</a> */}
+              </li>
             </ul>
           </div>
         </nav>
