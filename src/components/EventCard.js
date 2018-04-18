1868ab1877c03a43478390a63deef7f7e820932c
Francky <gwadafrank@yahoo.com>
2018-04-18 13:37:24 +0300

    [Feature]Implement event details

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class EventCard extends Component {
   render() {
@@ -13,7 +14,8 @@ class EventCard extends Component {
             <p className="small">{this.props.date}</p>
             <br /> <i className="fa fa-map-marker fa-2x fa-fw" aria-hidden="true"></i>
             <p className="small">{this.props.location}</p>
-            <a href="view_details.html" className="button small expanded hollow">View more details</a>
+            <Link to="/" className="button small expanded hollow" >View more details</Link>
+            {/* <a href="view_details.html" className="button small expanded hollow">View more details</a> */}
           </div>
         </div>
       </div>
---
+++ src/components/EventCardDetails.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const EventCardDetails = () => {
+  return (
+    <div>
+      <section className="row wide event-container">
+        <div className="overlay"></div>
+        <h2 className="text-display">Color Festival</h2>
+        <h3 className="text-display">9th December 2017</h3>
+        <h4 className="text-display">Nairobi</h4>
+      </section>
+      <section className="row event-description">
+        <div className="column large-8 small-12">
+          <h3>Description</h3>
+          <p> Odit et sint temporibus facilis omnis molestiae et. Et laborum sint dolorem eveniet. Qui quaerat reprehenderit omnis provident. Necessitatibus blanditiis esse delectus ipsum. Non quibusdam quaerat laborum. Fugit ipsa possimus blanditiis possimus cumque perspiciatis. </p>
+        </div>
+        <div className="column large-3 small-12"> <a className="button expanded">RSVP</a>
+          <div className="row interactions">
+            <div className="right">
+              <li><a href="#"><i className="fa fa-pencil"></i> </a></li>
+              <li><a href="#"><i className="fa fa-trash-o"></i></a></li>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
