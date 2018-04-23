e6d0bc9321847099d63ebed5a27def5d9c1abeb3
Francky <gwadafrank@yahoo.com>
2018-04-23 13:30:33 +0300

    [Feature] Fetch event details

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -9,13 +9,13 @@ class EventCard extends Component {
       <div>
         <div className="column">
           <div className="callout">
-            {/* <img className="thumbnail" src="http://lorempixel.com/400/200/nightlife" alt="event_image" /> */}
+            <img className="thumbnail" src="http://lorempixel.com/400/200/nightlife" alt="event_image" />
             <h5>{this.props.title }</h5>
             <i className="fa fa-calendar fa-2x fa-fw" aria-hidden="true"></i>
             <p className="small">{this.props.date}</p>
             <br /> <i className="fa fa-map-marker fa-2x fa-fw" aria-hidden="true"></i>
             <p className="small">{this.props.location}</p>
-            <Link className="button small expanded hollow" { values } to={`/events/${this.props.id}`} >
+            <Link className="button small expanded hollow" to={`/events/${this.props.id}`} >
               View more details
             </Link>
             {/* <a href="view_details.html" className="button small expanded hollow">View more details</a> */}
--- src/components/EventCardDetails.js
+++ src/components/EventCardDetails.js
@@ -1,32 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 
-const EventCardDetails = () => {
-  console.log("asdfghj");
-  return (
-    <div>
-      <section className="row wide event-container">
-        <div className="overlay"></div>
-        <h2 className="text-display">Color Festival</h2>
-        <h3 className="text-display">9th December 2017</h3>
-        <h4 className="text-display">Nairobi</h4>
-      </section>
-      <section className="row event-description">
-        <div className="column large-8 small-12">
-          <h3>Description</h3>
-          <p> Odit et sint temporibus facilis omnis molestiae et. Et laborum sint dolorem eveniet. Qui quaerat reprehenderit omnis provident. Necessitatibus blanditiis esse delectus ipsum. Non quibusdam quaerat laborum. Fugit ipsa possimus blanditiis possimus cumque perspiciatis. </p>
-        </div>
-        <div className="column large-3 small-12"> <a className="button expanded">RSVP</a>
-          <div className="row interactions">
-            <div className="right">
-              <li><a href="#"><i className="fa fa-pencil"></i> </a></li>
-              <li><a href="#"><i className="fa fa-trash-o"></i></a></li>
+class EventCardDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id:'',
+      event: '',
+      location:'',
+      category:'',
+      date:'',
+    }
+  }
+
+  componentDidMount() {
+    const ROOT = "http://127.0.0.1:5000/api";
+    let eventId = this.props.match.params.id;
+    // console.log("id", eventId);
+    axios
+      .get(`${ROOT}/events/` + eventId.toString())
+      .then(response => {
+        // toastr.success(response.data.message);
+        console.log(response.data.event);
+        this.setState(
+          {
+            ...this.state,
+            id: response.data.id,
+            event: response.data.event,
+            location: response.data.location,
+            category: response.data.category,
+            date: response.data.date,
+          },
+          () => {
+            console.log(" main", this.state);
+          }
+        );
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <section className="row wide event-container">
+          <div className="overlay"></div>
+          <h2 className="text-display">{this.state.event}</h2>
+          <h3 className="text-display">{this.state.date}</h3>
+          <h4 className="text-display">{this.state.location}</h4>
+        </section>
+        <section className="row event-description">
+          <div className="column large-8 small-12">
+            <h3>Description</h3>
+            <p> Odit et sint temporibus facilis omnis molestiae et. Et laborum sint dolorem eveniet. Qui quaerat reprehenderit omnis provident. Necessitatibus blanditiis esse delectus ipsum. Non quibusdam quaerat laborum. Fugit ipsa possimus blanditiis possimus cumque perspiciatis. </p>
+          </div>
+          <div className="column large-3 small-12"> <a className="button expanded">RSVP</a>
+            <div className="row interactions">
+              <div className="right">
+                <li><a href="#"><i className="fa fa-pencil"></i> </a></li>
+                <li><a href="#"><i className="fa fa-trash-o"></i></a></li>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+        </section>
+      </div>
+    );
+  }
+}
 
 export default EventCardDetails;
 
