4f91cc4038cf21d7ea15bbc5f58d81c8289e5822
Francky <gwadafrank@yahoo.com>
2018-05-07 22:48:06 +0300

    [CHore]Update the event card link to pass whole state

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 class EventCard extends Component {
   render() {
+    // console.log(this.state.id);
     // console.log('props', this.props.id);
     const values = this.props;
     return (
       <div>
         <div className="column">
           <div className="callout">
-            <img className="thumbnail" src="http://lorempixel.com/400/200/nightlife" alt="event_image" />
-            <h5>{this.props.title }</h5>
-            <i className="fa fa-calendar fa-2x fa-fw" aria-hidden="true"></i>
-            <p className="small">{this.props.date}</p>
-            <br /> <i className="fa fa-map-marker fa-2x fa-fw" aria-hidden="true"></i>
-            <p className="small">{this.props.location}</p>
-            <Link className="button small expanded hollow" to={`/events/${this.props.id}`} >
+            {/* <img className="thumbnail" src="http://lorempixel.com/400/200/nightlife" alt="event_image" /> */}
+            <h5>{values.title}</h5>
+            <i className="fa fa-calendar fa-2x fa-fw" aria-hidden="true" />
+            <p className="small">{values.date}</p>
+            <br /> <i className="fa fa-map-marker fa-2x fa-fw" aria-hidden="true" />
+            <p className="small">{values.location}</p>
+            <Link className="button small expanded hollow" to={{ pathname: `/events/${values.id}`, state: { event: values } }}>
               View more details
             </Link>
-            {/* <a href="view_details.html" className="button small expanded hollow">View more details</a> */}
           </div>
         </div>
       </div>
--- src/components/EventCardDetails.js
+++ src/components/EventCardDetails.js
@@ -16,80 +16,118 @@ class EventCardDetails extends Component {
       location: '',
       category: '',
       date: '',
+      token: ''
     };
+    // const eventId = this.props.match.params.id;
   }
 
   componentDidMount() {
-    const eventId = this.props.match.params.id;
-    axios
-      .get(`${ROOT}/events/${  eventId.toString()}`)
-      .then(response => {
-        // console.log(response.data.event);
-        this.setState(
-          {
-            ...this.state,
-            id: response.data.id,
-            event: response.data.event,
-            location: response.data.location,
-            category: response.data.category,
-            date: response.data.date,
-          },
-          () => {
-          },
-        );
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    // const eventId = this.props.match.params.id;
+    // axios
+    //   .get(`${ROOT}/events/${  eventId.toString()}`)
+    //   .then(response => {
+    //     // console.log(response.data.event);
+    //     // this.setState(
+    //     //   {
+    //     //     ...this.state,
+    //     //     id: response.data.id,
+    //     //     event: response.data.event,
+    //     //     location: response.data.location,
+    //     //     category: response.data.category,
+    //     //     date: response.data.date,
+    //     //   },
+    //     //   () => {
+    //     //   },
+    //     // );
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
   }
 
   handleRsvp = () => {
     const eventId = this.props.match.params.id;
+    const token = localStorage.getItem("access_token")
+  
     instance.interceptors.request.use(tokenProvider({
-        getToken: () => localStorage.getItem("access_token")      
+        getToken: () => localStorage.getItem("access_token")
       }));
 
-    instance.post(`${ROOT}/events/${eventId.toString()}/rsvp/`).then((response) => {
+    instance.post(`${ROOT}/events/${eventId.toString()}/rsvp/`)
+    .then((response) => {
           toastr.success(response.data.message);
           console.log("hgv", response)
     }).catch(function(error) {
         toastr.warning(error.response.data.message);
         console.log(error.response.data.message);
-      });     
-    
-    // axios
-    //   .post(`${ROOT}/events/${eventId.toString()}/rsvp`)
-    //   .then(response => {
-    //     // toastr.success(response.data.message);
-    //     console.log(response.data)
-    //   })
-    //   .catch(function(error) {
-    //     toastr.warning(error.response.data.message);
-    //     console.log(error.response.data.message);
-    //   });
+      });
+  }
+  
+  handleEdit = () => {
+    console.log("edit it");
+  }
+  handleDelete = () =>{
+    console.log("event", this.props.location.state.event.id);
+
+    let token = localStorage.getItem("access_token");
+    let IDEvent = this.props.location.state.event.id;
+
+    instance.interceptors.request.use(tokenProvider({
+      getToken: () => localStorage.getItem("access_token")
+      }));
+    instance.delete(`${ROOT}/events/${IDEvent.toString()}`).then((response) => {
+          toastr.success(response.data.message);
+      setTimeout(function () {
+        window.location.assign("/");
+      }, 2000);
+          console.log("hgv", response)
+    }).catch(function(error) {
+        toastr.warning(error.response.data.message);
+        console.log(error.response.data.message);
+      });
+
   }
 
-
   render() {
+    // console.log("sdfggj", this.props.location.state.event);
+    const event = this.props.location.state.event;
+    
     return (
-      <div>
+    <div>
         <section className="row wide event-container">
-          <div className="overlay"></div>
-          <h2 className="text-display">{this.state.event}</h2>
-          <h3 className="text-display">{this.state.date}</h3>
-          <h4 className="text-display">{this.state.location}</h4>
+          <div className="overlay" />
+          <h2 className="text-display">{event.title}</h2>
+          <h3 className="text-display">{event.date}</h3>
+          <h4 className="text-display">{event.location}</h4>
         </section>
         <section className="row event-description">
           <div className="column large-8 small-12">
             <h3>Description</h3>
-            <p> Odit et sint temporibus facilis omnis molestiae et. Et laborum sint dolorem eveniet. Qui quaerat reprehenderit omnis provident. Necessitatibus blanditiis esse delectus ipsum. Non quibusdam quaerat laborum. Fugit ipsa possimus blanditiis possimus cumque perspiciatis. </p>
+            <p>
+              {" "}
+              Odit et sint temporibus facilis omnis molestiae et. Et laborum
+              sint dolorem eveniet. Qui quaerat reprehenderit omnis
+              provident. Necessitatibus blanditiis esse delectus ipsum. Non
+              quibusdam quaerat laborum. Fugit ipsa possimus blanditiis
+              possimus cumque perspiciatis.{" "}
+            </p>
           </div>
-          <div className="column large-3 small-12"> 
-          <button className="button expanded" onClick={this.handleRsvp}>RSVP</button>
+          <div className="column large-3 small-12">
+            <button className="button expanded" onClick={this.handleRsvp}>
+              RSVP
+            </button>
             <div className="row interactions">
               <div className="right">
-                <li><a href="#"><i className="fa fa-pencil"></i> </a></li>
-                <li><a href="#"><i className="fa fa-trash-o"></i></a></li>
+                <li>
+                  <a href="#" onClick={this.handleEdit}>
+                    <i className="fa fa-pencil" />{" "}
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={this.handleDelete}>
+                    <i className="fa fa-trash-o" />
+                  </a>
+                </li>
               </div>
             </div>
           </div>
