f70ca98ac967e0c1a63fea0abab252f6c7412b84
Francky <gwadafrank@yahoo.com>
2018-05-14 21:36:48 +0300

    [Test] Test the login components

---
+++ src/__tests__/login.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Enzym from "../setupTests";
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import Login from '../components/Login';
+
+
+describe('Login Component', () => {
+  const wrapper = shallow(<Login />);
+  const preventDefault = jest.fn();
+  it('renders without crashing', () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it('renders state initially', () => {
+    expect(wrapper.state().email).toEqual('');
+    expect(wrapper.state().password).toEqual('');
+  });
+});
