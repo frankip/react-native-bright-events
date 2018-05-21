ace0723ca2ae7753190e30a07c72b77d432a595b
Francky <gwadafrank@yahoo.com>
2018-05-22 01:02:12 +0300

    [feature] Implement Add event  modal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import DatePicker from "material-ui/DatePicker";
+import TextField from "material-ui/TextField";
 
 // local imports
 import EventCard from './EventCard';
@@ -28,7 +33,37 @@ class Main extends Component {
       });
   }
 
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleSubmit = () => {
+    console.log("submit button");
+    console.log(this.firstname);
+    
+    this.setState({ open: false });
+    
+  };
+
   render() {
+    const action = [
+      <FlatButton
+        label="close"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.handleSubmit}
+      />,
+    ];
+
     const eventlist = this.state.eventList.map((event) => {
       return (
         <EventCard
@@ -45,12 +80,28 @@ class Main extends Component {
 
 
     return (
-      <div>
+    <div>
         <Navigation />
         <section className="row">
           <div id="eventCard" className="row small-up-1 medium-up-2 large-up-3">
             {eventlist}
           </div>
+        
+        <FloatingActionButton iconClassName="add" label="add event" onClick={this.handleOpen} />
+
+          <Dialog 
+          title="Create a New Event" 
+            actions={action} 
+            modal={true}
+            open={this.state.open} 
+            onRequestClose={this.handleClose}
+            autoScrollBodyContent={true}
+          >
+            <TextField floatingLabelText="Event Name" floatingLabelFixed={true} ref="first_name"  /> <br /> <br />\
+            <TextField floatingLabelText="Description" /> <br /><br />
+            <TextField floatingLabelText="Event Location" floatingLabelFixed={true} /> <br /><br />
+            <DatePicker hintText="Event date" /><br />
+          </Dialog>
         </section>
       </div>
     );
