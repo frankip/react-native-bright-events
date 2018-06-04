cbee4d81cd7afef3a3e40f30b468013efebc9d59
Francky <gwadafrank@yahoo.com>
2018-06-04 23:40:43 +0300

    [Feature] check if token is expired

--- src/components/url_config.js
+++ src/components/url_config.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import tokenProvider from 'axios-token-interceptor';
+import jwtDecode from 'jwt-decode';
 
 
 export const ROOT = 'http://127.0.0.1:5000/api';
@@ -8,3 +9,12 @@ export const instance = axios.create({});
 instance.interceptors.request.use(tokenProvider({
   getToken: () => localStorage.getItem('access_token'),
 }));
+
+export const isTokenExpired = token => {
+  const currentTime = new Date() / 1000;
+  if (jwtDecode(token).exp < currentTime) {
+    localStorage.clear();
+    return true;
+  }
+  return false;
+};
