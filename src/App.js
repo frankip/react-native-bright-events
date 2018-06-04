919255420f00bd17515d13d7184848d6ac9ba0cd
Francky <gwadafrank@yahoo.com>
2018-06-04 21:37:31 +0300

    [feature] implement context provider

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import Approutes from './Routes';
 import './static/App.css';
 import Navigation from './components/Navigation';
 
+// instanciate new context
+export const MyContext = React.createContext();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      token: '',
+
+    };
+  }
+
+  handleUpdateState = (data) => {
+    this.setState({ isLoggedIn: data.loggedin, token: data.token });
+  }
+
   render() {
     return (
-      <Router>
-        <div className="App">
-          {/* <Navigation /> */}
-          <Approutes />
-        </div>
-      </Router>
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          updateState: this.handleUpdatestate,
+      }}
+      >
+        {this.props.children}
+        <Router>
+          <div className="App">
+            {/* <Navigation /> */}
+            <Approutes />
+          </div>
+        </Router>
+      </MyContext.Provider>
     );
   }
 }
