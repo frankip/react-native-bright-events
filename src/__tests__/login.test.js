707d0ca3e64ee9d4b5f7080563f96d0c83e2caba
Francky <gwadafrank@yahoo.com>
2018-06-07 18:09:31 +0300

    [test] Add login test

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,16 +1,40 @@
 
+import "jsdom-global/register";
 import React from 'react';
-import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
+import Enzyme, { mount, shallow } from 'enzyme';
+import ReactRouterEnzymeContext from 'react-router-enzyme-context';
+import Adapter from 'enzyme-adapter-react-16';
 import sinon from 'sinon';
 import expect from 'expect';
 import Login from '../components/Login';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 const fakeSubmit = sinon.spy();
 const fakeOnChange = sinon.spy();
 
+function setup() {
+  const options = new ReactRouterEnzymeContext();
+  const props = {
+    state: {
+      email: '',
+      password: '',
+      loggedIn: false,
+      access_token: '',
+    },
+    onSubmit: fakeSubmit,
+    onChange: fakeOnChange,
+  };
+  return mount(
+    <Login {...props} />,
+    options.get(),
+  );
+}
+
 describe('Login Component', () => {
-  const wrapper = shallow(<Login />);
+  const wrapper = setup();
+
   // const preventDefault = jest.fn();
   // it('renders without crashing', () => {
   //   expect(shallowToJson(wrapper)).toMatchSnapshot();
@@ -30,7 +54,10 @@ describe('Login Component', () => {
   });
   it('simulates click onsubmit', () => {
     const form = wrapper.find('form');
-    // form.simulate('submit', { preventDefault() { } });
-    // expect(fakeSubmit.called).toBe(true);
+    form.simulate('submit', { preventDefault() { } });
+    expect(fakeSubmit.called).toBe(false);
+  });
+  it('should have a submit prop', () => {
+    expect(wrapper.prop('onSubmit')).toBeDefined();
   });
 });
